304dbc6334f21b9ef1b4b3e8f89562f54893f683
fentonhaslam <fentonhaslam@gmail.com>
2019-07-16 17:01:01 +0100

    added students

--- directory.rb
+++ directory.rb
@@ -15,11 +15,20 @@ students = [
 ]
 
 # first we print the list of students
-puts "The students of Villains Academy"
-puts "-------------------"
-students.each do |student|
-  puts student
+def print_header
+    puts "The students of Villains Academy"
+    puts "-------------------"
 end
-# finally, we print the total number of students
-puts "Overall, we have #{students.count} great students"
-# it's important that print() doesn't add new line characters
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+# Nothing happens until we call the mehtods created.
+
+print_header
+print(students)
+print_footer(students)
