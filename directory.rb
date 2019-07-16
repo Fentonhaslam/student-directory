078b163658e738758fab0758cc751cb1eb510e47
fentonhaslam <fentonhaslam@gmail.com>
2019-07-16 18:12:04 +0100

    Array of students with input

--- directory.rb
+++ directory.rb
@@ -1,32 +1,4 @@
-student_count = 11
 # Let's put all the students in an array
-students = [
-  { name: "Dr. Hannibal Lecter", cohort: :november },
-  { name: "Darth Vadar", cohort: :november},
-  { name: "Nurse Ratched", cohort: :november},
-  { name: "Michael Corleone", cohort: :november},
-  { name: "Alex Delarge", cohort: :november},
-  { name: "the wicked Witch of the West", cohort: :november},
-  { name: "Terminator", cohort: :november},
-  { name: "Freddy Krueger", cohort: :november},
-  { name: "The Joker", cohort: :november},
-  { name: "Joffrey Baratheon", cohort: :november},
-  { name: "Norman Bates", cohort: :november}
-]
-
-# first we print the list of students
-def print_header
-    puts "The students of Villains Academy"
-    puts "-------------------"
-end
-def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
-  end
-end
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
-end
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, hit return twice"
@@ -45,6 +17,22 @@ def input_students
   # return array of students
   students
 end
+# first we print the list of students
+def print_header
+    puts "The students of Villains Academy"
+    puts "-------------------"
+end
+
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
 # Nothing happens until we call the mehtods created.
 students = input_students
 print_header
