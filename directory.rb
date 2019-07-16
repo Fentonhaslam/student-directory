1f626e934bf94ad8efa7b74d40f7eb219b37c314
fentonhaslam <fentonhaslam@gmail.com>
2019-07-16 18:08:40 +0100

    added students

--- directory.rb
+++ directory.rb
@@ -27,8 +27,26 @@ end
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array.
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name
+    name = gets.chomp
+  end
+  # return array of students
+  students
+end
 # Nothing happens until we call the mehtods created.
-
+students = input_students
 print_header
 print(students)
 print_footer(students)
