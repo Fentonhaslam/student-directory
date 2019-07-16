1fc3bcfc3d50e2037383a5bc4546c0e69a68fc0d
fentonhaslam <fentonhaslam@gmail.com>
2019-07-16 17:20:06 +0100

    added students

--- directory.rb
+++ directory.rb
@@ -1,17 +1,17 @@
 student_count = 11
 # Let's put all the students in an array
 students = [
-  "Dr. Hannibal Lecter",
-  "Darth Vadar",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex Delarge",
-  "the wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates"
+  { name: "Dr. Hannibal Lecter", cohort: :november },
+  { name: "Darth Vadar", cohort: :november},
+  { name: "Nurse Ratched", cohort: :november},
+  { name: "Michael Corleone", cohort: :november},
+  { name: "Alex Delarge", cohort: :november},
+  { name: "the wicked Witch of the West", cohort: :november},
+  { name: "Terminator", cohort: :november},
+  { name: "Freddy Krueger", cohort: :november},
+  { name: "The Joker", cohort: :november},
+  { name: "Joffrey Baratheon", cohort: :november},
+  { name: "Norman Bates", cohort: :november}
 ]
 
 # first we print the list of students
@@ -19,9 +19,9 @@ def print_header
     puts "The students of Villains Academy"
     puts "-------------------"
 end
-def print(names)
-  names.each do |name|
-    puts name
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 def print_footer(names)
