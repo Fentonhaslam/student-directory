aaec7ad72265aa37a4546c0f5e95a21d5845000f
fentonhaslam <fentonhaslam@gmail.com>
2019-07-17 10:08:01 +0100

    save students file

--- directory.rb
+++ directory.rb
@@ -37,6 +37,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save students"
   puts "9. Exit"
 end
 
@@ -52,6 +53,8 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
      exit
    else
@@ -64,6 +67,18 @@ def interactive_menu
     print_menu
     process(gets.chomp)
   end
+end
+
+def save_students
+  # oepn file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name],student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 interactive_menu
