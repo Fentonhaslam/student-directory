5efc3cbdfc963ed218d677e66982fa46d4199d5a
fentonhaslam <fentonhaslam@gmail.com>
2019-07-17 10:51:27 +0100

    ARGV file add

--- directory.rb
+++ directory.rb
@@ -5,7 +5,7 @@ def input_students
   puts "To finish, hit return twice"
   # create an empty array
   # get the first name
-  name = gets.chomp
+  name = STDIN.gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array.
@@ -37,7 +37,8 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
-  puts "3. Save students"
+  puts "3. Save students to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -55,6 +56,8 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
      exit
    else
@@ -65,7 +68,7 @@ end
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
@@ -80,5 +83,27 @@ def save_students
   end
   file.close
 end
+
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(",")
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
+def try_load_students
+  filename = ARGV.first # first arguement from command line
+  return if filename.nil? # get out of the method if it isn't given
+  if File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry #{filename} doesn't exist."
+    exit
+  end # Quit the program
+end
+
 
 interactive_menu
